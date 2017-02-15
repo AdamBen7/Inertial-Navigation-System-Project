6684743f767634bc53b414ef8281ffa4901624d3
AdamBen7 <adam.benabbou@hotmail.com>
2017-02-15 16:28:58 -0600

    Added new Calibration Program + updatelocal.sh updated

--- updatelocal.sh
+++ updatelocal.sh
@@ -1,5 +1,6 @@
 ##Maybe use $variables for .ino files. Might be better practice
 cp INS.ino ../Arduino/INS/. 
+cp Calibration.ino ../Arduino/Calibration/. 
 cp -a GY-85-arduino-master ../Arduino/libraries
 cp -a QuadcopterLib ../Arduino/libraries
 
